41d2d334c0f9fff0816d46b38b1b50612f0e19e6
abderrezak <abderrezak.khif@gmail.com>
2020-03-23 10:42:16 +0100

    add a test to check my settings

---
+++ src/test/java/cashRegister/CashRegisterTest.java
@@ -0,0 +1,11 @@
+package cashRegister;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CashRegisterTest {
+
+    @Test
+    public void first(){
+        Assertions.assertThat(true).isTrue();
+    }
+}
